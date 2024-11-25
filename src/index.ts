19031e09dec5bf67ef9f1cb977c39e35c5425275
eekelof <erik.ekelof1@gmail.com>
2024-11-25 03:34:56 +0100

    fixed docs

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,26 @@ export interface V3 extends V2 {
     z: number;
 }
 
-export function getAngle(from: V2, to: V2): number {
+export function getAngle(from: V2, to: V2) {
     return Math.atan2(to.y - from.y, to.x - from.x);
 }
-export function getDst2(p1: V2, p2: V2): number {
+export function getDst2(p1: V2, p2: V2) {
     const dx = p2.x - p1.x;
     const dy = p2.y - p1.y;
     return dx * dx + dy * dy;
 }
-export function getDst(p1: V2, p2: V2): number {
+export function getDst(p1: V2, p2: V2) {
     return getDst2(p1, p2) ** 0.5;
 }
-export function pointCircleCol(p1: V2, p2: V2, r: number): boolean {
+export function pointCircleCol(p1: V2, p2: V2, r: number) {
     return getDst2(p1, p2) < r * r;
 }
 
 /**
- * @param {V2} a Pos for the start of the line
- * @param {V2} b Pos for the end of the line
- * @param {V2} c Pos of the circle
- * @param {number} r Radius of the circle
+ * @param {V2} a Line start pos
+ * @param {V2} b Line end pos
+ * @param {V2} c Circle pos
+ * @param {number} r Circle radius
  * @returns {boolean} True if line is colliding with circle
  */
 export function lineCircleCol(a: V2, b: V2, c: V2, r: number): boolean {
@@ -37,10 +37,10 @@ export function lineCircleCol(a: V2, b: V2, c: V2, r: number): boolean {
 }
 
 /**
- * @param {V2} a Pos for the start of the line
- * @param {V2} b Pos for the end of the line
- * @param {V2} c Pos of the point
- * @returns {boolean} Pos on the line a-b closest to c
+ * @param {V2} a Line start pos
+ * @param {V2} b Line end pos
+ * @param {V2} c Point pos
+ * @returns {V2} Pos on the line a-b closest to c
  */
 export function getClosestPointOnLine(a: V2, b: V2, c: V2): V2 {
     const dx = b.x - a.x;
@@ -63,9 +63,10 @@ export function getClosestPointOnLine(a: V2, b: V2, c: V2): V2 {
     return { x, y };
 }
 
-export function isOutsideSquare(pos: V2, size: number): boolean {
+export function isOutsideSquare(pos: V2, size: number) {
     return pos.x > size || pos.x < 0 || pos.y > size || pos.y < 0;
 }
+
 /**
  * Converts a hex color to rgb
  * @param color Color in hex
